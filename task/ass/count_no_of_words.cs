fd1608643cb32f253b9bca93b2038c533e40019f
bharath <bharath.250390@yahoo.co.in>
2013-12-04 23:52:59 +0530

    Update count_no_of_words.cs

--- task/ass/count_no_of_words.cs
+++ task/ass/count_no_of_words.cs
@@ -1,5 +1,3 @@
-//read each word and count the number of times the word is appearing
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,57 +6,68 @@ using System.IO;
 
 namespace ConsoleApplication7
 {
- class Program
+    class calc
+    {
+
+        public static void Wcount(string Fpath)
+        {
+            if (File.Exists(Fpath))
+            {
+
+                //splitting the words using space, punctuation marks
+                string[] Word = System.IO.File.ReadAllText(Fpath).ToLower().Split(new char[] { ' ', ',', ';', '.', '-' });
+
+
+                //comparing the word with rest of the words
+                for (int i = 0; i < Word.Length; i++)
+                {
+                    int count = 0;
+                    for (int j = 0; j < Word.Length; j++)
+                    {
+                        if (Word[i] == Word[j]) // if the word matches then the count will be added
+                        {
+                            count++;    
+                        }
+                    }
+                    Console.WriteLine("{0}   {1}", Word[i], count);
+                }
+
+                /*
+
+                 var count = Word
+                .GroupBy(x => x)
+                .Select(g => new { Word = g.Key, Count = g.Count() })
+                .ToList();
+
+                 foreach (var p in count)
+                 {
+                     Console.WriteLine("{0} {1}", p.Word, p.Count);
+                 }
+                */
+
+
+
+
+
+            }
+            else
+                Console.WriteLine("File not exist");
+
+        }
+    }
+
+
+  class WordCount
 {
  static void Main(string[] args)
  {
-     
      Console.WriteLine("Enter the file path");
      string path = Console.ReadLine();
-
-     if (File.Exists(path))
-     {
-
-
-         string[] text = System.IO.File.ReadAllText(path).ToLower().Split(new char[] { ' ', ',', ';', '.', '-' });
-
-
-
-         for (int i = 0; i < text.Length; i++)
-         {
-             int count = 0;
-             for (int j = 0; j < text.Length; j++)
-             {
-                 if (text[i] == text[j])
-                 {
-                     count++;
-                 }
-             }
-             Console.WriteLine("{0}   {1}", text[i], count);
-         }
-
-         /*
-
-          var count = text
-         .GroupBy(x => x)
-         .Select(g => new { text = g.Key, Count = g.Count() })
-         .ToList();
-
-          foreach (var p in counts)
-          {
-              Console.WriteLine("{0} {1}", p.text, p.Count);
-          }
-
-
-
-          */
-
-
-     }
-     else
-         Console.WriteLine("File not exist");
-
- }
+     calc calc= new calc();
+  
+     calc.Wcount(path);
+  
+      }
 
   }
 
